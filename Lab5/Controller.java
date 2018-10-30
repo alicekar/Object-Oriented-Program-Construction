2f2e5d174d7f17dda36a21f8510346766fadf3a1
alka <alice.karnsund@gmail.com>
2018-10-30 17:06:12 +0100

    30-10-18 17:05

--- Lab5/Controller.java
+++ Lab5/Controller.java
@@ -3,35 +3,85 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.net.*;
+import javax.swing.event.*;
+
 /**
  *
  * @author alicekarnsund
  * 
  */	
-public class Controller{
+public class Controller implements HyperlinkListener, ActionListener, KeyListener{
 
 	View myView = new View(this);
 
 
 	public Controller(){
-		try{
-			myView.myPane.setPage(new URL("https://www.kth.se"));
-		}
-		catch(java.io.IOException ioe){
-			System.out.println("IOException while loading KTH homepage");
-		}
-
+        try{
+            hyperlinkUpdate(new HyperlinkEvent(this, 
+                    HyperlinkEvent.EventType.ACTIVATED, new URL("http://kth.se")));
+		}catch (MalformedURLException e){
+			myView.errorMessage("There is something wrong with the home page");
+        } 
 	}
 
  	/** The close-button closes when being clicked
      * @param closeIn
-     * @param frameIn */
+     * @param frameIn 
+     */
 	public void actionClose(JButton closeIn, JFrame frameIn){
 		closeIn.addActionListener(e -> frameIn.dispose());
 	}
 
+	/** hyperlinkUpdate is a function which goes to the URL corresponding to the 
+     * link clicked inside the editorPane
+     * @param e 
+     * @Override
+     */
+    public void hyperlinkUpdate(HyperlinkEvent e){
+    	if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED){
+    		myView.setPage(e.getURL());
+    	}
+    }	
+
+	/** keyTyped is an auto generated method which comes with keyListener
+	 * @param e 
+	 * @Override
+	 */
+    public void keyTyped(KeyEvent e){
+    }
+     
+    /** keyPressed is an auto generated method which comes with keyListener 
+     * @param e 
+	 * @Override
+     */
+    public void keyPressed(KeyEvent e){
+    }
+     
+    /** Handle the key released event from the text field. 
+     * @param e 
+	 * @Override
+     */
+    public void keyReleased(KeyEvent e){
+    	if (e.getKeyCode() == KeyEvent.VK_ENTER){
+    		String address = myView.getAddress();
+    		try{
+    			URL url = new URL(address);
+    			myView.setPage(url);
+    		}catch(MalformedURLException ex){
+    			myView.errorMessage("The URL you have entered is invalid! Please "
+                    + "try again");
+    		}		
+		}	
+    }
+     
+    /** Handle the button click. */
+    public void actionPerformed(ActionEvent e){
+    }
+
+
 	/** Main method, calls the constructor and runs the program
-     * @param args */
+     * @param args 
+     */
 	public static void main(String[] args) {
 		new Controller();
 	}
--- Lab5/View.java
+++ Lab5/View.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
+import java.net.*;
+
 
 /**
  *
@@ -10,38 +12,96 @@ import java.io.*;
  * the most basic functions for the browser
  */	
 public class View extends JFrame{
-	//private JLabel myLabel = new JLabel("Test");
 	private JPanel topPanel = new JPanel();
-	private JTextField myTextField = new JTextField("Enter Address");
-	private JButton myClose = new JButton("Close");
-	public JEditorPane myPane = new JEditorPane();
+	private JTextField myTextField = new JTextField(55);
+	private JButton closeButton = new JButton("CLOSE");
+	private JButton backButton = new JButton("  <  ");
+	private JButton forwardButton = new JButton("  >  ");
+	private JEditorPane myPane = new JEditorPane();
+	private JMenuBar myMenu = new JMenuBar();
+	private JMenu menuText = new JMenu(" HISTORY ");
+	private JMenuItem menuItem = new JMenuItem("Item");
 	//private Container container = getContentPane();   // Needed to add thing to the frame
-	//Controller myController = new Controller();
 
 	/** Constructor */ 
 	public View(Controller controllerIn){
 		super("The Web Browser");
 
-		controllerIn.actionClose(myClose, this);
-
 		/* Set up the top panel and the editor with the components*/
-	    //topPanel.add(myLabel);
+		backButton.setEnabled(false);
+		forwardButton.setEnabled(false);
+		myMenu.add(menuText);
+		menuText.add(menuItem);
+		menuText.setEnabled(false);
+		myTextField.addKeyListener(controllerIn);
+
+		topPanel.add(backButton);
+		topPanel.add(forwardButton);
+		topPanel.add(myMenu);
 		topPanel.add(myTextField);
-		topPanel.add(myClose);
+		topPanel.add(closeButton);
+
 		myPane.setContentType("text/html");
 		myPane.setEditable(false);
-		//myPane.setBorder(BorderFactory.createLineBorder(Color.red));  // To test the position of panel2
+		myPane.addHyperlinkListener(controllerIn);
+		controllerIn.actionClose(closeButton, this);
 
 		/* Set up the frame and its components*/
 		add(topPanel, BorderLayout.NORTH);
 		add(new JScrollPane(myPane), BorderLayout.CENTER);
-
-		/* Set up the Frame */
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setPreferredSize(new Dimension(400, 300));
-		pack();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(screenSize);
+		setResizable(false);
 		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	/** Returns the text in the text field as a string
+	 *  @return
+	 */
+	public String getAddress(){
+		String address = myTextField.getText();
+		return address;
+	}
+
+	/** Shows the current URL in the editorPane
+	 *
+	 */
+	public void setPage(URL urlIn){
+		try{
+			myPane.setPage(urlIn);
+		}catch(IOException e){}
+	}
+
+	/** The function enableForward enables the forward button if the user  
+     * has gone back to an earlier website either by using the back button or by
+     * the history menu.
+     * @param booleanIn 
+	 */
+	public void enableForward(boolean booleanIn){
+		forwardButton.setEnabled(booleanIn);
+	}
+
+    /** The function enableBack enables the back button if there has been 
+     * one or more sites visited before the current one.
+     * @param booleanIn 
+     */
+	public void enableBack(boolean booleanIn){
+		backButton.setEnabled(booleanIn);
+	}
+
+    /** The function enableMenu enables the history menu if there has been sites
+     * visited before the current one.
+     * @param booleanIn 
+     */
+    public void enableMenu(boolean booleanIn){
+    	menuText.setEnabled(booleanIn);
+    }
+
+
+	public void errorMessage(String messageIn){
+		JOptionPane.showMessageDialog(this, messageIn, "Warning", 
+			JOptionPane.ERROR_MESSAGE);
+	}
 
 }
