f911f804daf8312bd08ef864988b4728d0926591
alka <alice.karnsund@gmail.com>
2018-11-05 16:45:17 +0100

    Final version

--- Lab5/Controller.java
+++ Lab5/Controller.java
@@ -1,66 +1,66 @@
+//package lab5;
+
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.net.*;
 import javax.swing.event.*;
-import java.util.*;
-import java.net.HttpURLConnection;
 
 /**
  *
  * @author alicekarnsund
+ * @author elinsamuelsson
  * 
  * The controller class implements 3 types of listeners that are necessary for
  * interactions with the program. It contains the functions that translates the
  * users interactions with myView into actions 
  */	
 public class Controller implements HyperlinkListener, ActionListener, KeyListener{
-	View myView = new View(this);
-	Model myModel = new Model();
+    private View myView = new View(this);
+    private Model myModel = new Model();
 
-	/** Constructor */
-	public Controller(){
+    /** Constructor */
+    public Controller(){
         try{
             hyperlinkUpdate(new HyperlinkEvent(this, 
-                    HyperlinkEvent.EventType.ACTIVATED, new URL("http://kth.se")));
-		}catch (MalformedURLException e){
-			myView.errorMessage("There is something wrong with the home page");
+                HyperlinkEvent.EventType.ACTIVATED, new URL("http://kth.se")));
+            }catch (MalformedURLException e){
+		myView.errorMessage("There is something wrong with the home page");
         } 
-	}
+    }
 
- 	/** The close-button closes the frame when being clicked
-     * @param closeIn
-     * @param frameIn 
+    /** The close-button closes the frame when being clicked
+     * @param closeIn the close button 
+     * @param frameIn the frame where all is set up
      */
-	public void actionClose(JButton closeIn, JFrame frameIn){
-		closeIn.addActionListener(e -> frameIn.dispose());
-	}
+    public void actionClose(JButton closeIn, JFrame frameIn){
+	closeIn.addActionListener(e -> frameIn.dispose());
+    }
 
-	/** hyperlinkUpdate is a function which goes to the URL corresponding to the 
+    /** hyperlinkUpdate is a function which goes to the URL corresponding to the 
      * link clicked inside the editorPane
-     * @param e 
-     * @Override
+     * @param e event when a hyperlink is klicked
      */
+    @Override
     public void hyperlinkUpdate(HyperlinkEvent e){
     	if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED){
-    		myModel.addAddress(e.getURL());
-    		myView.setPage(e.getURL());
-    		updateButtons();
+            myModel.addAddress(e.getURL());
+            myView.setPage(e.getURL());
+            updateButtons();
     	}
     }	
 
-	/** keyTyped is an auto generated method which comes with keyListener
-	 * @param e 
-	 * @Override
-	 */
+    /** keyTyped is an auto generated method which comes with keyListener
+     * @param e key event when a key on the keyboard is typed 
+     */
+    @Override
     public void keyTyped(KeyEvent e){
     }
      
     /** keyPressed is an auto generated method which comes with keyListener 
-     * @param e 
-	 * @Override
+     * @param e key event when a key on the keyboard is pressed
      */
+    @Override
     public void keyPressed(KeyEvent e){
     }
      
@@ -68,9 +68,9 @@ public class Controller implements HyperlinkListener, ActionListener, KeyListene
      * when "enter" is pressed and then the URL is being entered with the goTo 
      * method in View2 if the response code == 200. If the URL is invalid, 
      * an error message is shown.
-     * @param e 
-     * @Override
+     * @param e key event when a key on the keyboard is released
      */
+    @Override
     public void keyReleased(KeyEvent e){
     	if (e.getKeyCode() == KeyEvent.VK_ENTER){
     		String address = myView.getAddress();
@@ -93,14 +93,15 @@ public class Controller implements HyperlinkListener, ActionListener, KeyListene
     			myView.errorMessage("The URL you have entered is invalid! Please "
                     + "try again");
     		}		
-		}
+	}
     }
      
     /** The function actionPerformed notices if one has clicked the back or 
      * forward button and then enters the matching site, or else it enters the
      * url choosen in the history list.
-     * @param e 
+     * @param e an action is made, i.e a button is clicked
      */
+    @Override
     public void actionPerformed(ActionEvent e){
     	if(e.getActionCommand()=="  <  "){
     		URL previous = myModel.previousAddress();
@@ -138,7 +139,7 @@ public class Controller implements HyperlinkListener, ActionListener, KeyListene
     }
 
 	/** Main method, calls the constructor and runs the program
-     * @param args 
+     * @param args to run the program
      */
 	public static void main(String[] args) {
 		new Controller();
--- Lab5/Model.java
+++ Lab5/Model.java
@@ -1,106 +1,109 @@
+//package lab5;
+
 import java.net.URL;
 import java.util.*;
 import java.net.*;
-import java.util.Date;
 import java.io.IOException;
 
 /**
  *
  * @author alicekarnsund
+ * @author elinsamuelsson
  *
  * The Model class contains the hidden functions, such as keeping tranck of number
  * of websites visited and at which website in the addressList we are at the moment
  */
 public class Model{
-	ArrayList<URL> addressList;
-	int currentIdx = -1;
+    ArrayList<URL> addressList;
+    int currentIdx = -1;
 
-	/** Constructor
-	 * Creates a new ArrayList where all the visited websites will be stored
-	 */
-	public Model(){
-		addressList = new ArrayList<URL>();
-	}
+    /** Constructor
+     * Creates a new ArrayList where all the visited websites will be stored
+     */
+    public Model(){
+	addressList = new ArrayList<URL>();
+    }
 
-	/** The function addAdress adds a new address to the addressList and calls the 
-	 * forward function to increase the index by one
-	 * @param urlIn
-	 */
-	public void addAddress(URL urlIn){
-		while(currentIdx<addressList.size()-1 && currentIdx!=-1){
-			addressList.remove(addressList.size()-1);
-		}
-		addressList.add(urlIn);
-		forward();
+    /** The function addAdress adds a new address to the addressList and calls the 
+     * forward function to increase the index by one
+     * @param urlIn url of a given new website
+     */
+    public void addAddress(URL urlIn){
+        while(currentIdx<addressList.size()-1 && currentIdx!=-1){
+            addressList.remove(addressList.size()-1);
 	}
+        addressList.add(urlIn);
+	forward();
+    }
 
-	/** The function getAddressList, returns the whole list of visited addresses
-     * @return
-	 */
-	public ArrayList<URL> getAddressList(){
-		return addressList;
-	}
+    /** The function getAddressList, returns the whole list of visited addresses
+     * @return the whole address list
+     */
+    public ArrayList<URL> getAddressList(){
+	return addressList;
+    }
 
     /** The function hasNext, returns true if one has visited and address 
      * after the current one
-     * @return
-	 */
-	public boolean hasNext(){
-		return currentIdx < addressList.size()-1;
-	}
+     * @return true if there is a website that has been visited after this one
+     */
+    public boolean hasNext(){
+	return currentIdx < addressList.size()-1;
+    }
 
     /** The function hasPrevious, returns true if one has visited and address 
      * prior to the current one
-     * @return
-	 */
-	public boolean hasPrevious(){
-		return currentIdx > 0;
-	}
+     * @return true if there is a website that has been visited prior this one
+     */
+    public boolean hasPrevious(){
+	return currentIdx > 0;
+    }
 
-	/** The function nextAddress, returns the address visited after 
-     * @return
-	 */
-	public URL nextAddress(){
-		return addressList.get(currentIdx+1);
-	}
+    /** The function nextAddress, returns the address visited after 
+     * @return the next url in the address list
+     */
+    public URL nextAddress(){
+	return addressList.get(currentIdx+1);
+    }
 
     /** The function previousAddress, returns the address visited earlier
-     * @return
-	 */
-	public URL previousAddress(){
-		return addressList.get(currentIdx-1);
-	}
+     * @return the previous url in the address list
+     */
+    public URL previousAddress(){
+	return addressList.get(currentIdx-1);
+    }
 
     /** The forward-function, increases the current index in the addressList
-	 */
-	public void forward(){
-		currentIdx++;
-	}
+     */
+    public void forward(){
+	currentIdx++;
+    }
 
-	/** The back-function, decreases the current index in the addressList
-	 */
-	public void back(){
-		currentIdx--;
-	}
+    /** The back-function, decreases the current index in the addressList
+     */
+    public void back(){
+	currentIdx--;
+    }
 
-	/** The function indexOf, finds the index in the addressList of an URL
-	 * @param urlIn
-	 */
-	public void indexOf(URL urlIn){
-		currentIdx = addressList.indexOf(urlIn);
-	}
+    /** The function indexOf, finds the index in the addressList of an URL
+     * @param urlIn any url one wish to find the position of in the address list
+     */
+    public void indexOf(URL urlIn){
+	currentIdx = addressList.indexOf(urlIn);
+    }
 
-	/** The checkResponseCode function checks the response code of the url
-	 * to make sure it is valid, ie == 200. Otherwise an exception uis thrown
-	 * and an error message is shown.
-	 * @param urlIn
-	 * @return
-	 */
-	public static int checkResponseCode(URL urlIn) throws IOException{
-		HttpURLConnection httpCon = (HttpURLConnection)urlIn.openConnection();
-	    System.out.println("Response code is " + httpCon.getResponseCode());
-	    return httpCon.getResponseCode();
-	}
+    /** The checkResponseCode function checks the response code of the url
+     * to make sure it is valid, ie == 200. Otherwise an exception uis thrown
+     * and an error message is shown.
+     * @param urlIn url to see if it is valid
+     * @return the response code
+     * @throws IOException caught in controller when testing a bad url
+     */
+    public static int checkResponseCode(URL urlIn) throws IOException{
+       	HttpURLConnection httpCon = (HttpURLConnection)urlIn.openConnection();
+	System.out.println("Response code is " + httpCon.getResponseCode());
+	return httpCon.getResponseCode();
+    }
 
 
 
